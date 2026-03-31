ed439109e827714a4d071f70fed19e67822d410f
GUILHERME PEREIRA FRANCO <guilherme.franco005@cs.cruzeirodosul.edu.br>
2026-03-30 21:21:50 -0300

    segundo commit

---
+++ index-server.js
@@ -0,0 +1,11 @@
+// import http from 'http';
+const { createServer } = require('http')
+let server = createServer((request,response)=>{
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write('<h1>HELLO,WORD</h1>');
+    response.end();
+})
+
+server.listen(3002);
+console.log("iniciado")
+
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-let msg = "Hello word"
+let express = require("express");
+let app = express();
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+    res.send("home")
+})
+app.get('/produtos', (req, res)=> {
+    res.send("PRODUTO 1")
+})
+
+app.route('/books')
+        .get(function (params) {
+            res.send("get book")
+        })
+        .post(function () {
+             res.send("Cad livro")
+        }).
+
+app.listen(PORT, function (params) {
+    console.log(`Server funando na porta ${PORT}`)
+})
