96a6818ba17478f8cb77132b4c2761d6a3069948
GUILHERME PEREIRA FRANCO <guilherme.franco005@cs.cruzeirodosul.edu.br>
2026-04-13 21:14:12 -0300

    banco rodando e respondendo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let cors = require("cors");
 const db = require("./db")
+
 let app = express();
 app.use(express.json());
 app.use(cors());
@@ -8,26 +9,53 @@ app.use(cors());
 const PORT = 3002;
 
 let usuarios = [
-    {id:1,nome:"claudinei"},
-    {id:2,nome:"rodisnei"}
+    { id: 1, nome: "claudinei" },
+    { id: 2, nome: "rodisnei" }
 ]
 
-app.get('/usuarios',(request, response)=>{
-    response.status(202).json({"dados":usuarios})
+app.post('/alunos', (req, res) => {
+    const { nome } = req.body;
+    const sql = "INSERT INTO ALUNOS (nome) values (?)"
+    db.query(sql, [nome], function (err, result) {
+        if (err) {
+            return response.status(500).json({ err: err.message })
+        }
+        res.status(200).json({
+            id:result.insertId,
+            nome
+        })
+    })
+
 })
 
-app.post('/usuarios',(req, res)=>{
+
+// app.get('/usuarios',(request, response)=>{
+//     let id = request.body.id
+//     let nome = request.body.nome;
+//     response.status(202).json({"dados":usuarios})
+// })
+
+app.get('/alunos', function (req,) {
+    db.query("SELECT * FROM escola.alunos", (err, result) => {
+        if (err) {
+            return response.status(erro).json({ err: err.message })
+        }
+        res.json(result)
+    })
+})
+
+app.post('/usuarios', (req, res) => {
     const nome = req.body.nome;
 
     const novouser = {
-        id:usuarios.length+1,
+        id: usuarios.length + 1,
         nome: nome
     }
 
     usuarios.push(novouser)
     console.log(nome)
     console.log(usuarios)
-    res.json({'nome enviado ':nome, 'users':usuarios})
+    res.json({ 'nome enviado ': nome, 'users': usuarios })
 })
 
 app.listen(PORT, function (params) {
